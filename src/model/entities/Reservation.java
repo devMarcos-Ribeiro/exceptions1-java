47b0cbbef68083b63b33587d5c230fa81691d32e
Marcos Gabriel Ribeiro Souto <marcosgrsouto@gmail.com>
2020-06-24 19:36:44 -0300

    Good Solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -12,7 +14,10 @@ public class Reservation {
 	private Date checkIn;
 	private Date checkOut;
 	
-	public Reservation(int roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(int roomNumber, Date checkIn, Date checkOut) throws DomainException{
+		if(!checkOut.after(checkIn)){
+			throw new DomainException("Reservation dates for updates must be future dates!");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -40,28 +45,27 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);	
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 		Date now = new Date();
 		if(checkIn.before(now)||checkOut.before(now)) {
-			return "Error in reservation: Reservation dates for updates must be future dates.";
+			throw new DomainException("Reservation dates for updates must be future dates.");
 		} 
 		if(!checkOut.after(checkIn)){
-			return "Error in reservation: Reservation dates for updates must be future dates!";
+			throw new DomainException("Reservation dates for updates must be future dates!");
 		}
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	
 	@Override
 	public String toString() {
-		return "Reservation: Room "+ this.getRoomNumber() 
-		+ ", check-in: " 
-		+ sdf.format(this.getCheckIn()) 
-		+ ", check-out: " 
-		+ sdf.format(this.getCheckOut()) 
-		+ ", " 
+		return "Room "+ this.getRoomNumber()
+		+ ", check-in: "
+		+ sdf.format(this.getCheckIn())
+		+ ", check-out: "
+		+ sdf.format(this.getCheckOut())
+		+ ", "
 		+ duration() 
 		+ " nights";
 	}
